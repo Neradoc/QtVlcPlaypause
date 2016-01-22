81f5e5052ebaa7ae4dde9f14f3c3d886e2d16588
Spyro <web@sinonrien.fr>
2016-01-22 23:11:43 +0100

    Initial Commit

---
+++ QtVlcPlaypause/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QtVlcPlaypause
+//
+//  Created by Toussaint Guglielmi on 22/01/2016.
+//  Copyright © 2016 Spyroland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
